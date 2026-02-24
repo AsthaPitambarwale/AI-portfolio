123a3992e098069774ec9053a23f0afdcc9e311c
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2026-02-24 15:16:48 +0530

    Initial Commit

--- frontend/src/components/AboutMe.tsx
+++ frontend/src/components/AboutMe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const AboutMe = () => {
   const [showInfo, setShowInfo] = useState(false); // ✅ State for toggle
